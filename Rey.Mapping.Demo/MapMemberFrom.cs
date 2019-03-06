6f6af56a45ba06d4d658d5c03a5cd112f00968ce
caizhifei2003 <caizhifei2003@163.com>
2019-03-06 12:32:13 +0800

    update

--- Rey.Mapping.Demo/MapMemberFrom.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Rey.Mapping {
-    public abstract class MapMemberFrom : MapFrom {
-        protected object Target { get; }
-
-        public MapMemberFrom(object target) {
-            this.Target = target;
-        }
-    }
-}
--- Rey.Mapping.Demo/Program.cs
+++ Rey.Mapping.Demo/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Rey.Mapping {
     class Program {
@@ -42,200 +44,57 @@ namespace Rey.Mapping {
         public int Age;
     }
 
-    //public abstract class MapValue {
-    //    public MapTypes Type { get; }
-    //    public object Value { get; }
+    #region 反射
 
-    //    public MapValue(MapTypes type, object value) {
-    //        this.Type = type;
-    //        this.Value = value;
-    //    }
-    //}
+    public class MapType {
+        public Type Type { get; }
+        public string Name => this.Type.Name;
 
-    //public class MapStringValue : MapValue {
-    //    public MapStringValue(string value)
-    //        : base(MapTypes.String, value) {
-    //    }
-    //}
+        public MapType(Type type) {
+            this.Type = type;
+        }
 
-    //public class MapNumeberValue : MapValue {
-    //    public MapNumeberValue(decimal value)
-    //        : base(MapTypes.Number, value) {
-    //    }
-    //}
+        public static implicit operator MapType(Type type) {
+            return new MapType(type);
+        }
 
-    //public class MapObjectValue : MapValue {
-    //    public MapObjectValue(object value)
-    //        : base(MapTypes.Object, value) {
-    //    }
-    //}
+        public IEnumerable<MapProperty> GetProperties(BindingFlags flags = BindingFlags.Public | BindingFlags.Instance) {
+            return this.Type.GetProperties(flags).Select(x => new MapProperty(x));
+        }
 
-    //public class MapPath {
-    //    public string Name { get; }
-    //    public MapPath Parent { get; }
+        public IEnumerable<MapField> GetFields(BindingFlags flags = BindingFlags.Public | BindingFlags.Instance) {
+            return this.Type.GetFields(flags).Select(x => new MapField(x));
+        }
 
-    //    public MapPath(string name = null, MapPath parent = null) {
-    //        this.Name = name;
-    //        this.Parent = parent;
-    //    }
-    //}
+        public IEnumerable<MapMember> GetMembers(BindingFlags flags = BindingFlags.Public | BindingFlags.Instance) {
+            var members = new List<MapMember>();
+            members.AddRange(this.GetProperties(flags));
+            members.AddRange(this.GetFields(flags));
+            return members;
+        }
+    }
 
-    ////public class MapNode {
-    ////    public MapValue Value { get; set; }
-    ////    public MapPath Path { get; set; }
-    ////    public List<MapNode> Children { get; } = new List<MapNode>();
-    ////}
+    public abstract class MapMember {
+        public abstract string Name { get; }
+    }
 
-    //public enum MapNodeTypes {
-    //    String,
-    //    Number,
-    //    Enum,
-    //    Date,
-    //    Object,
-    //}
+    public class MapField : MapMember {
+        public FieldInfo Field { get; }
+        public override string Name => this.Field.Name;
 
-    //public abstract class MapNode {
-    //    public MapNodeTypes NodeType { get; }
+        public MapField(FieldInfo field) {
+            this.Field = field;
+        }
+    }
 
-    //    public MapNode(MapNodeTypes nodeType) {
-    //        this.NodeType = nodeType;
-    //    }
-    //}
+    public class MapProperty : MapMember {
+        public PropertyInfo Property { get; }
+        public override string Name => this.Property.Name;
 
-    //public abstract class MapValueNode : MapNode {
-    //    public object Value { get; }
+        public MapProperty(PropertyInfo property) {
+            this.Property = property;
+        }
+    }
 
-    //    public MapValueNode(MapNodeTypes nodeType, object value)
-    //        : base(nodeType) {
-    //        this.Value = value;
-    //    }
-    //}
-
-    //public class MapStringValueNode : MapValueNode {
-    //    public MapStringValueNode(string value)
-    //        : base(MapNodeTypes.String, value) {
-    //    }
-    //}
-
-    //public class MapObjectNode : MapNode {
-    //    public MapObjectNode()
-    //        : base(MapNodeTypes.Object) {
-    //    }
-    //}
-
-    //public interface IMapFromContext {
-    //    MapNode MapFrom(MapFrom from);
-    //}
-
-    //public class MapFromContext : IMapFromContext {
-    //    private IDefaultFromMapper Mapper { get; }
-
-    //    public MapFromContext(IDefaultFromMapper mapper) {
-    //        this.Mapper = mapper;
-    //    }
-
-    //    public MapNode MapFrom(MapFrom from) {
-    //        return this.Mapper.MapFrom(from, this);
-    //    }
-    //}
-
-    //public class DefaultFromMapper : IDefaultFromMapper {
-    //    private IEnumerable<IFromMapper> Mappers { get; }
-
-    //    public DefaultFromMapper(IEnumerable<IFromMapper> mappers) {
-    //        this.Mappers = mappers;
-    //    }
-
-    //    public MapNode MapFrom(MapFrom from, IMapFromContext context) {
-    //        foreach (var mapper in this.Mappers) {
-    //            try {
-    //                return mapper.MapFrom(from, context);
-    //            } catch (MapFieldException) {
-    //                continue;
-    //            }
-    //        }
-    //        throw new MapFieldException();
-    //    }
-    //}
-
-    //public interface IFromMapper {
-    //    MapNode MapFrom(MapFrom from, IMapFromContext context);
-    //}
-
-    //public interface IDefaultFromMapper : IFromMapper {
-
-    //}
-
-    //public class MapFieldException : Exception {
-
-    //}
-
-    //public class FromStringMapper : IFromMapper {
-    //    public bool Filter(MapFrom from) {
-    //        var type = from.Type.Type;
-    //        return typeof(string).Equals(type)
-    //            || typeof(char).Equals(type);
-    //    }
-
-    //    public MapNode MapFrom(MapFrom from, IMapFromContext context) {
-    //        if (!this.Filter(from))
-    //            throw new MapFieldException();
-
-    //        var value = new MapStringValue($"{from.Value}");
-    //        return new MapNode() {
-    //            Value = value,
-    //        };
-    //    }
-    //}
-
-    //public class FromNumberMapper : IFromMapper {
-    //    public bool Filter(MapFrom from) {
-    //        var type = from.Type.Type;
-    //        return typeof(Int16).Equals(type) || typeof(UInt16).Equals(type)
-    //            || typeof(Int32).Equals(type) || typeof(UInt32).Equals(type)
-    //            || typeof(Int64).Equals(type) || typeof(UInt64).Equals(type)
-    //            || typeof(float).Equals(type) || typeof(double).Equals(type) || typeof(decimal).Equals(type);
-    //    }
-
-    //    public MapNode MapFrom(MapFrom from, IMapFromContext context) {
-    //        if (!this.Filter(from))
-    //            throw new MapFieldException();
-
-    //        var changed = (decimal)Convert.ChangeType(from.Value, typeof(decimal));
-    //        var value = new MapNumeberValue(changed);
-    //        return new MapNode() {
-    //            Value = value,
-    //        };
-    //    }
-    //}
-
-    //public class FromObjectMapper : IFromMapper {
-    //    public bool Filter(MapFrom from) {
-    //        var type = from.Type.Type;
-    //        return type.IsClass;
-    //    }
-
-    //    public MapNode MapFrom(MapFrom from, IMapFromContext context) {
-    //        if (!this.Filter(from))
-    //            throw new MapFieldException();
-
-    //        if (from.Value == null)
-    //            return null;
-
-    //        var node = new MapNode() {
-
-    //        };
-
-    //        var members = from.Type.GetMembers();
-    //        foreach (var member in members) {
-    //            var memberFrom = member.CreateMapFrom(from.Value);
-    //            var childNode = context.MapFrom(memberFrom);
-    //            if (childNode == null)
-    //                continue;
-
-    //            node.Children.Add(childNode);
-    //        }
-    //        return node;
-    //    }
-    //}
+    #endregion 反射
 }
