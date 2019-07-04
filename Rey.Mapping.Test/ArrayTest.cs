7b4b25c67ac7665f6a692650d8a8acf2a562320d
Kevin Cai <caizhifei2003@163.com>
2019-07-04 18:07:32 +0800

    array

---
+++ Rey.Mapping.Test/ArrayTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Rey.Mapping.Test {
+    public class ArrayTest : TestBase {
+        [Fact]
+        public void Test() {
+            var arr = new[] { 1, 2, 3, 4, 5 };
+
+            Assert.Equal(arr, this.Mapper.From(arr).To<int[]>());
+            Assert.Equal(arr, this.Mapper.From(arr).To<IEnumerable<int>>());
+
+            Assert.Equal(arr, this.Mapper.From(arr.ToList()).To<int[]>());
+            Assert.Equal(arr, this.Mapper.From(arr.ToList()).To<IEnumerable<int>>());
+        }
+    }
+}
