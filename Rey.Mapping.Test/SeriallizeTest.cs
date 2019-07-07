cab6642527fff897e3416a63fda700163861687a
Kevin Cai <caizhifei2003@163.com>
2019-07-07 21:06:01 +0800

    序列化和反序列化选项测试用例

---
+++ Rey.Mapping.Test/SeriallizeTest.cs
@@ -0,0 +1,82 @@
+﻿using Rey.Mapping.Test.Models;
+using Xunit;
+
+namespace Rey.Mapping.Test {
+    public class SeriallizeTest : TestBase {
+        [Fact]
+        public void TestIgnore() {
+            var person = new PersonFrom() {
+                Name = "Person",
+                Parent = new PersonFrom {
+                    Name = "Person Parent",
+                    Parent = new PersonFrom {
+                        Name = "Person Parent Parent"
+                    }
+                }
+            };
+
+            var to = this.Mapper.From(person).To<PersonTo>();
+            Assert.Equal("Person", to.Name);
+            Assert.Equal("Person Parent", to.Parent.Name);
+            Assert.Equal("Person Parent Parent", to.Parent.Parent.Name);
+
+            to = this.Mapper.From(person, opts => {
+                opts.Ignore(x => x.Name);
+            }).To<PersonTo>();
+
+            Assert.Null(to.Name);
+            Assert.Equal("Person Parent", to.Parent.Name);
+            Assert.Equal("Person Parent Parent", to.Parent.Parent.Name);
+
+            to = this.Mapper.From(person, opts => {
+                opts.Ignore(x => x.Parent.Name);
+            }).To<PersonTo>();
+
+            Assert.Equal("Person", to.Name);
+            Assert.Null(to.Parent.Name);
+            Assert.Equal("Person Parent Parent", to.Parent.Parent.Name);
+
+            to = this.Mapper.From(person, opts => {
+                opts.Ignore(x => x.Parent.Parent.Name);
+            }).To<PersonTo>();
+
+            Assert.Equal("Person", to.Name);
+            Assert.Equal("Person Parent", to.Parent.Name);
+            Assert.Null(to.Parent.Parent.Name);
+        }
+
+        [Fact]
+        public void TestMap() {
+            var person = new PersonFrom() {
+                Name = "Person",
+                Parent = new PersonFrom {
+                    Name = "Person Parent",
+                    Parent = new PersonFrom {
+                        Name = "Person Parent Parent"
+                    }
+                }
+            };
+
+            var to = this.Mapper.From(person).To<PersonTo>();
+            Assert.Equal("Person", to.Name);
+            Assert.Equal("Person Parent", to.Parent.Name);
+            Assert.Equal("Person Parent Parent", to.Parent.Parent.Name);
+
+            to = this.Mapper.From(person, opts => {
+                opts.Map(x => x.Name, "Parent.Name");
+            }).To<PersonTo>();
+
+            Assert.Null(to.Name);
+            Assert.Equal("Person", to.Parent.Name);
+            Assert.Equal("Person Parent Parent", to.Parent.Parent.Name);
+
+            to = this.Mapper.From(person, opts => {
+                opts.Map(x => x.Name, "Parent.Name", "Parent.Parent.Name");
+            }).To<PersonTo>();
+
+            Assert.Null(to.Name);
+            Assert.Equal("Person", to.Parent.Name);
+            Assert.Equal("Person", to.Parent.Parent.Name);
+        }
+    }
+}
